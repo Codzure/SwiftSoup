14c16484a03ccc8cd1c3ba7cad5895266a366ad7
Leonard Mutugi Muthoni <leonard.mutugi@equitybank.co.ke>
2022-12-19 22:14:08 +0300

    Initial Commit

---
+++ SwiftSoup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftSoup
+//
+//  Created by Leonard Mutugi on 19/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
